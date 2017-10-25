efafa86ab20bc808f5b81c86eef70c75c84997ce
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2017-10-26 00:04:28 +0200

    ciudad

--- pages/ciudad/ciudad-controller.js
+++ pages/ciudad/ciudad-controller.js
@@ -1,5 +1,129 @@
-app.controller('EspaciosController', function($scope, $rootScope, $http, config) {  
+app.controller('CiudadController', function($scope, $rootScope, $http, config) {  
 
 	$("body > header nav").removeClass("short");
+
+
+	// filters
+
+	$("section#ciudad-map aside .handle").click(function(){
+		$(this).parent().toggleClass('expanded');
+	});
+
+
+    // Google Maps
+
+    var myOptions = {
+        zoom: 13,
+        center: new google.maps.LatLng(41.3821797,2.1764733),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true,
+        zoomControl: true,
+        styles: [
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#193341"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#2c5a71"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#29768a"
+            },
+            {
+                "lightness": -37
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#406d80"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#406d80"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#3e606f"
+            },
+            {
+                "weight": 2
+            },
+            {
+                "gamma": 0.84
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "weight": 0.6
+            },
+            {
+                "color": "#1a3541"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#2c5a71"
+            }
+        ]
+    }
+]
+    };
+    var map = new google.maps.Map(document.getElementById("map"), myOptions); 
+
 
 });
