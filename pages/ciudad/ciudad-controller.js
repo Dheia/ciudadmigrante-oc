47e0eee5dae6ccae13eac7d4c3779b1a11b7b259
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2017-11-07 16:31:11 +0100

    mapa

--- pages/ciudad/ciudad-controller.js
+++ pages/ciudad/ciudad-controller.js
@@ -127,21 +127,70 @@ app.controller('CiudadController', function($scope, $rootScope, $http, config) {
 ]
     };
     var map = new google.maps.Map(document.getElementById("map"), myOptions); 
-    
+
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(41.3900844,2.1763873),
+        map: map,
+        icon: 'images/marker-blue.png'
+    });
+    var marker2 = new google.maps.Marker({
+        position: new google.maps.LatLng(41.3831942,2.1864975),
+        map: map,
+        icon: 'images/marker-green.png'
+    });
+    var marker3 = new google.maps.Marker({
+        position: new google.maps.LatLng(41.3800844,2.1663873),
         map: map,
         icon: 'images/marker-yellow.png'
     });
 
+
+    var content = [
+        '<h2>hola</h2><p>Dirección completa con calle número xxx puerta. 08000 - ciudad</p><p class="lead">T. <a href="tel:+34 600 000 000">+34 600 000 000</a></p><p class="lead"><a href="mailto:direccion@demail.com">direccion@demail.com</a></p><p class="lead"><a href="http://www.bcn.cat/novaciutadania/index.html" target="_blank">www.bcn.cat/novaciutadania/index.html</a></p>',
+        '<a href="javascript:" class="espacio"><img src="dummy-data/infowindow/espacio.jpg" alt=""><h2>Dialogos sin fronteras</h2></a>',
+        '<a href="javascript:" class="relato"><img src="dummy-data/infowindow/relato.jpg" alt=""></a>'        
+    ];
+
     var infowindow = new google.maps.InfoWindow({
-        content: 'hola'
     });
 
     google.maps.event.addListener(marker, 'click', function() {
-       infowindow.open(map, marker);
+        infowindow.setContent(content[0]);
+        infowindow.open(map, marker);
+    });
+    google.maps.event.addListener(marker2, 'click', function() {
+        infowindow.setContent(content[1]);
+        infowindow.open(map, marker2);
+    });
+    google.maps.event.addListener(marker3, 'click', function() {
+        infowindow.setContent(content[2]);
+        infowindow.open(map, marker3);
     });
 
-    infowindow.open(map, marker);
+    /*
+     * The google.maps.event.addListener() event waits for
+     * the creation of the infowindow HTML structure 'domready'
+     * and before the opening of the infowindow defined styles
+     * are applied.
+     */
+    google.maps.event.addListener(infowindow, 'domready', function() {
+
+        // Reference to the DIV which receives the contents of the infowindow using jQuery
+        var iwOuter = $('.gm-style-iw');
+
+        /* The DIV we want to change is above the .gm-style-iw DIV.
+        * So, we use jQuery and create a iwBackground variable,
+        * and took advantage of the existing reference to .gm-style-iw for the previous DIV with .prev().
+        */
+        var iwBackground = iwOuter.prev();
+
+        // Remove the background shadow DIV
+        // iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+
+        // // Remove the white background DIV
+        // iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+
+    });
+
 
 });
