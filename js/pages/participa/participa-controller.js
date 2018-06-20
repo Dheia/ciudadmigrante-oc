d5c8600c1c2a26693e329bdf4ca957570a0910e0
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2018-06-20 20:32:45 +0200

    fixes

--- js/pages/participa/participa-controller.js
+++ js/pages/participa/participa-controller.js
@@ -100,49 +100,45 @@ app.controller('ParticpaController', function($scope, $rootScope, $http, config,
 
     setTimeout(function(){
 
-        if (!$rootScope.isKiosk) {
+        // Google Maps
+        var map = new google.maps.Map($("#participa-map")[0], config.map); 
 
-            // Google Maps
-            var map = new google.maps.Map($("#participa-map")[0], config.map); 
+        var input = document.getElementById('participa-direccion');
+        var autocomplete = new google.maps.places.Autocomplete(input);
 
-            var input = document.getElementById('participa-direccion');
-            var autocomplete = new google.maps.places.Autocomplete(input);
+        // Bind the map's bounds (viewport) property to the autocomplete object,
+        // so that the autocomplete requests use the current map bounds for the
+        // bounds option in the request.
+        autocomplete.bindTo('bounds', map);
 
-            // Bind the map's bounds (viewport) property to the autocomplete object,
-            // so that the autocomplete requests use the current map bounds for the
-            // bounds option in the request.
-            autocomplete.bindTo('bounds', map);
+        marker = new google.maps.Marker({
+            map: map,
+            anchorPoint: new google.maps.Point(0, -29),
+            icon: 'images/marker-blue.png',
+            animation: google.maps.Animation.DROP
+        });
 
-            marker = new google.maps.Marker({
-                map: map,
-                anchorPoint: new google.maps.Point(0, -29),
-                icon: 'images/marker-blue.png',
-                animation: google.maps.Animation.DROP
-            });
+        autocomplete.addListener('place_changed', function() 
+        {
+            marker.setVisible(false);
+            var place = autocomplete.getPlace();
+            if (!place.geometry) {
+                // User entered the name of a Place that was not suggested and
+                // pressed the Enter key, or the Place Details request failed.
+                window.alert("No details available for input: '" + place.name + "'");
+                return;
+            }
 
-            autocomplete.addListener('place_changed', function() 
-            {
-                marker.setVisible(false);
-                var place = autocomplete.getPlace();
-                if (!place.geometry) {
-                    // User entered the name of a Place that was not suggested and
-                    // pressed the Enter key, or the Place Details request failed.
-                    window.alert("No details available for input: '" + place.name + "'");
-                    return;
-                }
-
-                // If the place has a geometry, then present it on a map.
-                if (place.geometry.viewport) {
-                    map.fitBounds(place.geometry.viewport);
-                } else {
-                    map.setCenter(place.geometry.location);
-                    map.setZoom(17);  // Why 17? Because it looks good.
-                }
-                marker.setPosition(place.geometry.location);
-                marker.setVisible(true);
-            });
-
-        }
+            // If the place has a geometry, then present it on a map.
+            if (place.geometry.viewport) {
+                map.fitBounds(place.geometry.viewport);
+            } else {
+                map.setCenter(place.geometry.location);
+                map.setZoom(17);  // Why 17? Because it looks good.
+            }
+            marker.setPosition(place.geometry.location);
+            marker.setVisible(true);
+        });
 
     }, 0);
 
